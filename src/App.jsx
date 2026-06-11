56c5640394f7783b698e235ec30356558bf0431c
apavlik02-blip <apavlik02@icloud.com>
2026-06-11 10:43:19 -0500

    PassPro full build

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Passpro React</h1>
+      <p>Welcome to your React application!</p>
+    </div>
+  )
+}
