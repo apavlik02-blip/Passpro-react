b5cc0db22f21b48c98d2bf33d92ffaebdb3d70a2
Apavlik02 <TheCozyWellnessCollective@gmail.com>
2026-09-11 05:22:57 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ViteSitemap from 'vite-plugin-sitemap' // 1. Import the plugin
 
 export default defineConfig({
   plugins: [
-    react(),
-    ViteSitemap({ // 2. Add the dynamic sitemap settings
-      hostname: 'https://passpro-react.vercel.app', // Swap out with custom domain later
-      readable: true,
-      routes: [
-        '/',
-        '/blog',
-        '/blog/how-hard-is-the-wisconsin-insurance-exam',
-        '/blog/wisconsin-insurance-exam-practice-questions',
-        '/blog/wisconsin-life-health-insurance-license-guide'
-      ]
-    })
+    react()
   ]
 })
-
