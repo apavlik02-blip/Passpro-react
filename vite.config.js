fc947b26009ce2c0d86e4474ad503bb06d52e864
Apavlik02 <TheCozyWellnessCollective@gmail.com>
2026-09-23 07:39:31 -0500

    Fix plugins array syntax in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [plugins: [react(), tailwindcss()],
-    react()
-  ]
+  plugins: [react(), tailwindcss()],
 })
